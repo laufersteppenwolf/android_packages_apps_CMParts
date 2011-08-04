b0cf858d412373c63db2ab4835dc15e8d69a7f13
doixanh <doixanh@gmail.com>
2011-08-04 09:57:24 +0200

    add setting for hide avatar in message
    Change-Id: I4ba2e75931b45360b31ab41f06d1c9bc58a35c0f

--- src/com/cyanogenmod/cmparts/activities/GingerDXActivity.java
+++ src/com/cyanogenmod/cmparts/activities/GingerDXActivity.java
@@ -29,7 +29,7 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
     private static final String FLIPPING_DOWN_SNOOZES_ALARM_PREF = "pref_flipping_snoozes_alarm";
 	private static final String BACK_BUTTON_ENDS_CALL_PREF = "pref_back_button_ends_call";
 	private static final String TRANSPARENT_STATUS_BAR_PREF = "pref_transparent_status_bar";
-//	private static final String HIDE_AVATAR_MESSAGE_PREF = "pref_hide_avatar_message";
+	private static final String HIDE_AVATAR_MESSAGE_PREF = "pref_hide_avatar_message";
 //	private static final String DO_PROFILE_SCROLLING_PREF = "pref_do_profile_scrolling";
 //	private static final String DO_PROFILE_FLINGING_PREF = "pref_do_profile_flinging";
     private static final String CALL_ME_LOUDER_PREF = "pref_call_me_louder";
@@ -45,7 +45,7 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
     private CheckBoxPreference mFlippingDownMutesRinger;
     private CheckBoxPreference mFlippingDownSnoozesAlarm;
     private CheckBoxPreference mBackButtonEndsCall;
-//    private CheckBoxPreference mHideAvatarMessage;
+    private CheckBoxPreference mHideAvatarMessage;
 //    private CheckBoxPreference mDoProfileScrolling;
 //    private CheckBoxPreference mDoProfileFlinging;
     private CheckBoxPreference mCallMeLouder;
@@ -85,8 +85,8 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
         mBackButtonEndsCall = (CheckBoxPreference) prefSet.findPreference(BACK_BUTTON_ENDS_CALL_PREF);
         mBackButtonEndsCall.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.BACK_BUTTON_ENDS_CALL, 0) == 1);
 
-//		mHideAvatarMessage = (CheckBoxPreference) prefSet.findPreference(HIDE_AVATAR_MESSAGE_PREF);
-//		mHideAvatarMessage.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.HIDE_AVATAR_MESSAGE, 0) == 1);
+		mHideAvatarMessage = (CheckBoxPreference) prefSet.findPreference(HIDE_AVATAR_MESSAGE_PREF);
+		mHideAvatarMessage.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.HIDE_AVATAR_MESSAGE, 0) == 1);
 
         mTransparentStatusBarPref = (ListPreference) prefSet.findPreference(TRANSPARENT_STATUS_BAR_PREF);
         mTransparentStatusBarPref.setValue(String.valueOf(Settings.System.getInt(mContext.getContentResolver(),
@@ -128,10 +128,10 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
             Settings.System.putInt(getContentResolver(), Settings.System.BACK_BUTTON_ENDS_CALL, mBackButtonEndsCall.isChecked() ? 1 : 0);
             return true;
         }
-//        else if (preference == mHideAvatarMessage) {
-//            Settings.System.putInt(getContentResolver(), Settings.System.HIDE_AVATAR_MESSAGE, mHideAvatarMessage.isChecked() ? 1 : 0);
-//            return true;
-//        }
+        else if (preference == mHideAvatarMessage) {
+            Settings.System.putInt(getContentResolver(), Settings.System.HIDE_AVATAR_MESSAGE, mHideAvatarMessage.isChecked() ? 1 : 0);
+            return true;
+        }
 //        else if (preference == mDoProfileScrolling) {
 //            Settings.System.putInt(getContentResolver(), Settings.System.DO_PROFILE_SCROLLING, mDoProfileScrolling.isChecked() ? 1 : 0);
 //            return true;
@@ -151,7 +151,7 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
     	if (newValue != null) {
-    	   	// we have a lock screen preference change?
+    	   	// we have a list preference change?
 	        if (preference == mLedDisabledFromPref) {
 				int val = Integer.parseInt(String.valueOf(newValue));
             	Settings.System.putInt(mContext.getContentResolver(), Settings.System.NOTIFICATION_LIGHT_DISABLED_START, val);
