01df245237c9390e469005f6b561f3b0454347c6
Artem <artemchep@gmail.com>
2012-09-10 11:18:54 +0300

    Achep's alarm options (part 2/3)

--- src/com/cyanogenmod/cmparts/activities/GingerDXActivity.java
+++ src/com/cyanogenmod/cmparts/activities/GingerDXActivity.java
@@ -43,8 +43,22 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
     private static final String CENTER_CLOCK_STATUS_BAR_PREF = "pref_center_clock_status_bar";
     private static final String UPDATE_CHECK_HOUR_PREF = "pref_update_check_hour";
     private static final String PICK_UP_TO_CALL_PREF = "pref_pick_up_to_call";
+    
+    
+    // AChep's alarm options
+    private static final String ACHEP_ALARM_FLIP_ACTION_PREF = "pref_achep_alarm_flip_action";
+    private static final String ACHEP_ALARM_SHAKE_ACTION_PREF = "pref_achep_alarm_shake_action";
+    private static final String ACHEP_ALARM_MATH_QUESTIONS_ENABLED_PREF = "pref_achep_alarm_math_questions_enabled";
+    private static final String ACHEP_ALARM_INCREASING_VOLUME_ENABLED_PREF = "pref_achep_alarm_increasing_volume_enabled";
+    
 
     static Context mContext;
+    
+    // AChep's alarm options
+    private ListPreference mAlarmShakeActionPref;
+    private ListPreference mAlarmFlipActionPref;
+    private CheckBoxPreference mAlarmIncreasingVolume;
+    private CheckBoxPreference mAlarmMathQuestions;
 
     private ListPreference mLedDisabledFromPref;
     private ListPreference mLedDisabledToPref;
@@ -78,6 +92,24 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
         addPreferencesFromResource(R.xml.gingerdx_settings);
         
         PreferenceScreen prefSet = getPreferenceScreen();
+        
+        // AChep's alarm options
+        mAlarmShakeActionPref = (ListPreference) prefSet.findPreference(ACHEP_ALARM_SHAKE_ACTION_PREF);
+        mAlarmShakeActionPref.setValue(String.valueOf(Settings.System.getInt(mContext.getContentResolver(),
+        	Settings.System.ACHEP_ALARM_SHAKE_ACTION, 0)));
+        mAlarmShakeActionPref.setOnPreferenceChangeListener(this);
+        
+        mAlarmFlipActionPref = (ListPreference) prefSet.findPreference(ACHEP_ALARM_FLIP_ACTION_PREF);
+        mAlarmFlipActionPref.setValue(String.valueOf(Settings.System.getInt(mContext.getContentResolver(),
+        	Settings.System.ACHEP_ALARM_FLIP_ACTION, 0)));
+        mAlarmFlipActionPref.setOnPreferenceChangeListener(this);
+        
+        mAlarmMathQuestions = (CheckBoxPreference) prefSet.findPreference(ACHEP_ALARM_MATH_QUESTIONS_ENABLED_PREF);
+        mAlarmMathQuestions.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.ACHEP_ALARM_MATH_QUESTIONS_ENABLED, 0) == 1);
+        mAlarmIncreasingVolume = (CheckBoxPreference) prefSet.findPreference(ACHEP_ALARM_INCREASING_VOLUME_ENABLED_PREF);
+        mAlarmIncreasingVolume.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.ACHEP_ALARM_INCREASING_VOLUME_ENABLED, 0) == 1);
+        
+        
         
         mLedDisabledPref = (CheckBoxPreference) prefSet.findPreference(LED_DISABLED_PREF);
         mLedDisabledPref.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.NOTIFICATION_LIGHT_DISABLED, 0) == 1);
@@ -159,7 +191,18 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
     }
         
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) { 
+    	if (preference == mAlarmMathQuestions) {
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.ACHEP_ALARM_MATH_QUESTIONS_ENABLED, mAlarmMathQuestions.isChecked() ? 1 : 0);
+            return true;
+        }
+        else if (preference == mAlarmIncreasingVolume) {
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.ACHEP_ALARM_INCREASING_VOLUME_ENABLED, mAlarmIncreasingVolume.isChecked() ? 1 : 0);
+            return true;
+        }
+        else
         if (preference == mLedDisabledPref) {
             Settings.System.putInt(getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_DISABLED, mLedDisabledPref.isChecked() ? 1 : 0);
@@ -231,6 +274,14 @@ public class GingerDXActivity extends PreferenceActivity implements OnPreference
     public boolean onPreferenceChange(Preference preference, Object newValue) {
     	if (newValue != null) {
     	   	// we have a list preference change?
+	        if (preference == mAlarmShakeActionPref) {
+				int val = Integer.parseInt(String.valueOf(newValue));
+            	Settings.System.putInt(mContext.getContentResolver(), Settings.System.ACHEP_ALARM_SHAKE_ACTION, val);
+            }
+	        if (preference == mAlarmFlipActionPref) {
+				int val = Integer.parseInt(String.valueOf(newValue));
+            	Settings.System.putInt(mContext.getContentResolver(), Settings.System.ACHEP_ALARM_FLIP_ACTION, val);
+            }
 	        if (preference == mLedDisabledFromPref) {
 				int val = Integer.parseInt(String.valueOf(newValue));
             	Settings.System.putInt(mContext.getContentResolver(), Settings.System.NOTIFICATION_LIGHT_DISABLED_START, val);
